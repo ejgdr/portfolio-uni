0c9fc10b18e0f5c03fb418ec649db25c96d6be44
ejgdr <gui20006@byui.edu>
2025-12-18 00:52:55 -0700

    update vite config to read my repository

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Use relative paths for GitHub Pages
+  base: '/portfolio-uni/', // Use relative paths for GitHub Pages
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
